6daa316b903d70e3bb0801d19654cf9935e0ebfc
John Musiu <johnmusiu@gmail.com>
2021-01-16 14:23:48 +0300

    Create a json 404 response for the API

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,7 +2,9 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\Routing\Exception\RouteNotFoundException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -36,5 +38,15 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+
+    }
+
+    public function render($request, Throwable $e)
+    {
+        if($e instanceof ModelNotFoundException)
+        {
+            return response()->json(['error' => $e->getMessage()], 404);
+        }
+        return parent::render($request, $e);
     }
 }
